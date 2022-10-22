543387b3ec4ae944082d1d6d6b0297b27b1e7291
Tyler Nickerson <nickersoft@gmail.com>
2022-10-22 13:44:43 -0700

    added reproduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { loadAllLocales } from '../i18n/i18n-util.sync';
+	import { LL, setLocale } from '../i18n/i18n-svelte';
+
+	loadAllLocales();
+	setLocale('en');
+</script>
+
+<p>{$LL.HI({ name: 'Ivan' })}</p>
